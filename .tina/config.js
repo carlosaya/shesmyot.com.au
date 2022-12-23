6458918e0c2bc301528611d02dcc317a03866197
Carlos Aya <carlos.aya@tabcorp.com.au>
2022-12-23 13:16:46 +1000

    use tinaCMS

---
+++ .tina/config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "15efd316-673d-480a-ad3b-a039750823d8", // Get this from tina.io
+  token: "e7be618bab11f1b73a64746d67a4ff1f6b397ef0", // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        label: "Pages",
+        name: "pages",
+        path: "content",
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
